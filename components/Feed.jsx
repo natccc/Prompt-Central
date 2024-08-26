8205c367f1e7878a5debc364b4bf22c7dcc2519a
Natalie <natchc@gmail.com>
2024-08-27 00:38:33 +0100

    attempt to fix feed not updating

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -47,7 +47,7 @@ const Feed = () => {
   useEffect(() => {
     const fetchPosts = async () => {
       try {
-        const response = await fetch("/api/prompt");
+        const response = await fetch("/api/prompt",{cache:"no-store"});
         const data = await response.json();
         setAllPosts(data);
       } catch (error) {
